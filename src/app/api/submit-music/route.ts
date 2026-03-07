0b100a85a8425c1e6f6bc3ffb9fffd5cd2c251e7
Idowu Josiah <jazzthedev@Idowus-MacBook-Pro.local>
2026-03-07 21:02:48 +0100

    fix body size limit

--- src/app/api/submit-music/route.ts
+++ src/app/api/submit-music/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import cloudinary, { UploadApiResponse } from "../../../lib/cloudinary";
 
+
+export const maxDuration = 60;
+
 export async function POST(req: NextRequest) {
     try {
         const formData = await req.formData();
